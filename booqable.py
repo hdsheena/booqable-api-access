bea9be3ffb5fb06ee5cefc1c4a5f257c9c810a05
courtney <courtney@inventoryally.com>
2022-10-29 11:51:59 -0700

    generalized methods

--- booqable.py
+++ booqable.py
@@ -16,34 +16,47 @@ settings_data = get_settings()
 token = settings_data["token"]
 urlbase = settings_data["urlbase"]
 
-url = "https://"+urlbase+".booqable.com/api/boomerang/product_groups"
+def get_items(itemType):
+    url = "https://"+urlbase+".booqable.com/api/boomerang/"+itemType
+    pagesize = "?page[size]=100"
 
-headers = {
-    "accept": "*/*",
-    "accept-language": "en-US,en;q=0.9,en-GB;q=0.8",
-    "content-type": "application/json",
-   "authorization": "Bearer "+token
-  }
+    headers = {
+        "accept": "*/*",
+        "accept-language": "en-US,en;q=0.9,en-GB;q=0.8",
+        "content-type": "application/json",
+       "authorization": "Bearer "+token
+      }
 
-data = {
-  "filters": [
-  ]
-}
+    data = {
+      "filters": [
+      ]
+    }
 
 
-# print("doing request next")
-# print(url+payload, headers)
-after = ""
-#url = "https://interior-trial.booqable.com/api/boomerang/product_groups/4ad65217-e5e0-4d17-bcde-083e516dba2b"
-response = requests.request("GET", url, headers=headers)
-print(response.content)
-print(response.headers)
-for i in response.json()["data"]:
-	print(i['id'])
-	deleteurl = url+"/"+i['id']
-	print(deleteurl)
-	response1 = requests.request("DELETE", deleteurl, headers=headers)
-	print(response1.content)
+    # print("doing request next")
+    # print(url+payload, headers)
+    after = ""
+    #url = "https://interior-trial.booqable.com/api/boomerang/product_groups/4ad65217-e5e0-4d17-bcde-083e516dba2b"
+    response = requests.request("GET", url+pagesize, headers=headers)
+    print(response.content)
+    print(response.headers)
+    data = response.json()["data"]
+    return data
+
+def delete_items(data):
+    for i in data:
+        #print(i['id'])
+        deleteurl = url+"/"+i['id']
+        #print(deleteurl)
+        response1 = requests.request("DELETE", deleteurl, headers=headers)
+        print(response1.content)
+        
+data = get_items("product_groups")
+data = get_items("products")
+for i in data:
+    print(i)
+#delete_items(data)
+
 
     
 
